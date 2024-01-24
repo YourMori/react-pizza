720917e63f479596157ef67f57f760a9569e39c8
YourMori <pa4ok_28@mail.ru>
2024-01-24 19:51:30 +0700

    made fetch (mockapi)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React from "react";
+
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
-import pizzas from "./assets/pizzas.json";
-
 import "./scss/app.scss";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://65b1037ad16d31d11bddd342.mockapi.io/pizzas")
+      .then((res) => res.json())
+      .then((arr) => {
+        setPizzas(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
