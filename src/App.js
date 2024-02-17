8f5e79b882aa2749bff481a95c70038435f41da2
YourMori <pa4ok_28@mail.ru>
2024-02-17 18:19:25 +0700

    added Outlet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Header from "./components/Header";
+import MainLayout from "./Layouts/MainLayout";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import PizzaInfo from "./pages/PizzaInfo";
@@ -11,19 +11,14 @@ import "./scss/app.scss";
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/pizza/:id" element={<PizzaInfo />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<PizzaInfo />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
